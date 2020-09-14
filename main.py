e73703ba138b6a6e16bb362514e5ee50dd5e6473
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2020-09-14 12:08:01 +0530

    commands line options finished

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import requests
 import time
 from datetime import datetime
+import argparse
 
 class BitcoinNotification:
     def __init__(self, Bitcoin_Api_URL, Webhooks_URL):
@@ -26,12 +27,14 @@ class BitcoinNotification:
         price = self.getBitcoinPrice()
         value = {'value1': price}
         self.postWebhooks('bitcoin_price_emergency', value)
+        print("Emergency notification sent")
 
     def sendTelegramNotificaton(self):
         
         price = self.getBitcoinPrice()
         value = {'value1': price}
         self.postWebhooks('bitcoin_price_update', value)
+        print("Telegram Notification sent")
         
         #time.sleep(5 * 60)  # Sleep for 5 minutes
 
@@ -40,11 +43,9 @@ class BitcoinNotification:
         name = input('Please enter your name: ')
         email = input('Please enter your Email: ')
         price = self.getBitcoinPrice()
-        
         value = {'value1':email, 'value2':name, 'value3': price}
-        
         self.postWebhooks('email_notification', value)
-
+        print("Email notification sent")
 
 
 
@@ -55,7 +56,63 @@ if __name__ == "__main__":
     Webhooks_URL = 'https://maker.ifttt.com/trigger/{}/with/key/no2QXAKRWEMzu572PNeau5QTtIA3TrOrm4oRjtBLLY6'
 
     b1 = BitcoinNotification(Bitcoin_Api_URL,Webhooks_URL)
+    current_bitcoinprice = b1.getBitcoinPrice()
     
-    b1.sendIFTTTEmergencyNotificaton()
-    #b1.sendTelegramNotificaton()
-    #b1.sendGmailNotification()
+##################################################################################################################
+
+
+    parser = argparse.ArgumentParser(
+    usage='''\
+        Usage: This app gives the price of 1 Bitcoin in INR. 
+        Destination(-d) must be provided else destination will be telegram by default.
+        To recive notification from IFTTT install IFTTT mobile app. To recive notifications on 
+        Telegram install Telegram app and join this channel @IFTTT.
+
+        Prerequisite : INSTALL IFTTT APP AND TELEGRAM APP ON ANDROID/IOS/WINDOWS TO RECIVE NOTIFICATON. 
+        PRESS Ctrl+C to terminate the app
+        ''',
+        description="Bitcoin price Notification",
+        epilog="Copyrights @ Rajat Bhatt")
+    # command line variable for time gap
+    parser.add_argument(
+        "-i",
+        "--interval",
+        type=int,
+        nargs=1,
+        metavar="interval",
+        default=[1],
+        help="Time interval in minutes")
+    # command line variable for threshold
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        type=int,
+        nargs=1,
+        metavar="threshold",
+        default=[400000],
+        help="Threshold in INR(₹)")
+        
+    # command line variable for destination
+    parser.add_argument(
+        "-d",
+        "--destination",
+        metavar='destnation',
+        default='telegram',
+        help='There are various options to recive notifications from us (1)IFTTT app (2) Telegram app (3) Email')
+    new_args = parser.parse_args()
+    print('Running Application with time interval of ', new_args.interval[0],
+          ' and threshold = ₹', new_args.threshold[0], 'and Destination = ',
+          new_args.destination)
+    # calls the function to send notifications
+    if (new_args.destination == 'telegram' and new_args.threshold[0] < current_bitcoinprice):
+        print('''\
+            To recive the notification
+        from Telegram, install the telegram app and join the
+        channel @IFTTT
+        ''')
+        b1.sendTelegramNotificaton()
+    
+    if (new_args.destination == 'ifttt' or new_args.threshold[0] > current_bitcoinprice):
+        b1.sendIFTTTEmergencyNotificaton()
+    if (new_args.destination == 'email'):
+        b1.sendGmailNotification()
